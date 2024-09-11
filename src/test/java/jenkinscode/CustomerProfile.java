c13a493edf6827a7576f6cd268d7014a10b21e43
divyajn12 <140383399+divyajn12@users.noreply.github.com>
2024-09-11 16:14:23 +0530

    Update CustomerProfile.java

--- src/test/java/jenkinscode/CustomerProfile.java
+++ src/test/java/jenkinscode/CustomerProfile.java
@@ -80,7 +80,7 @@ public class CustomerProfile {
 
 		// Enter Phone number.
 
-		driver.findElement(By.xpath("(//input[@formcontrolname='mobile'])")).sendKeys("9588233411");
+		driver.findElement(By.xpath("(//input[@formcontrolname='mobile'])")).sendKeys("97676767671");
 
 		// click on Date of birth column.
 
